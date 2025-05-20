e9e912f9d5ca10d287254b5bf30536117b0a4b89
Pussla <fergusmclaughlin136@gmail.com>
2025-05-20 17:29:46 +0100

    new menus added with a scene manager not fully linked up but basically done, will need to revisit this stuff but got the foundations in.

--- src/card/character_card.gd
+++ src/card/character_card.gd
@@ -1,3 +1,13 @@
 extends "res://src/card/card.gd"
 
 const isPlayerCard = true
+
+func updateCardVisuals():
+	super.updateCardVisuals()
+	
+	if cardHealth <= 0:
+		await get_tree().create_timer(0.5).timeout
+		characterDeath()
+
+func characterDeath():
+	SceneTransitionManager.transitionToScene("res://src/ui/gameOver/game_over.tscn", SceneTransitionManager.TransitionType.FADE)
--- src/ui/mainMenu/main_menu.gd
+++ src/ui/mainMenu/main_menu.gd
@@ -3,7 +3,11 @@ extends Node
 
 func _ready():
 	$QuitButton.connect("pressed", Callable(self, "on_quit_button_pressed"))
+	$StartButton.connect("pressed", Callable(self, "on_start_button_pressed"))
 
 func on_quit_button_pressed():
 	print("hello")
 	get_tree().quit()
+
+func on_start_button_pressed():
+	SceneTransitionManager.transitionToScene("res://src/tests/card_test.tscn", SceneTransitionManager.TransitionType.FADE)
