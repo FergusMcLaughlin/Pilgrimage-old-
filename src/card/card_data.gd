2691aa6a9996f15c8cc9f7ff1f3650a83b89bc59
Pussla <37308913+FergusMcLaughlin@users.noreply.github.com>
2025-11-21 17:55:10 +0000

    added cardData resourse for story 1

---
+++ src/card/card_data.gd
@@ -0,0 +1,12 @@
+class_name cardData
+extends Resource
+
+@export var cardId: String
+@export var cardName: String
+@export var cardType: String
+@export var cardHealth: int
+@export var cardAttack: int
+@export var cardIsPlayer: bool
+@export var cardIsUnlocked: bool
+@export var cardImagePath: String
+@export var cardEffects: Array[String] = []
