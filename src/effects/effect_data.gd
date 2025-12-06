bbf8fa73af98d0e2f579159accaded8e26353c6f
Pussla <37308913+FergusMcLaughlin@users.noreply.github.com>
2025-12-06 17:08:08 +0000

    added an effect resource

---
+++ src/effects/effect_data.gd
@@ -0,0 +1,9 @@
+extends Resource
+class_name EffectData
+
+@export var effectId: String #Change this to be an actual ID rather than a name.
+@export var effectName: String
+@export var effectTrigger: String
+@export var effectFrequency: String
+@export var effectType: String
+@export var effectParameters: Dictionary = {}
